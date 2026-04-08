edf8530b465146d297427d9f7888a2b67554a602
Meet Ramjiyani <meetramjiyani132@gmail.com>
2026-04-08 23:49:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-﻿from app import app
+from app import app
 
 # This file is for compatibility with the validator
 # The main FastAPI app is in app.py
+
+# Make sure app is exposed
+__all__ = ['app']
